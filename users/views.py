a80f4f6a8679d8c53a573a89ca419b28c3d80504
Owen <owen@wsos.com.tw>
2022-11-30 16:09:01 +0800

    修正登入問題

--- users/views.py
+++ users/views.py
@@ -14,22 +14,6 @@ from drf_spectacular.settings import spectacular_settings
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
-class Index(TemplateView):
-    def get(self, request):
-        if request.user.is_anonymous:
-            return HttpResponseRedirect('/accounts/login/')
-        return render(request, 'base.html')
-
-class AvatarChange(TemplateView):
-    def get(self, request):
-        form = UserProfileForm(instance=request.user.userprofile)
-        return render(request, 'avatar_change.html', {'form': form})
-
-    def post(self, request):
-        form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile) # instance 指定列
-        if form.is_valid():
-            form.save()
-        return render(request, 'avatar_change.html', {'form': form})
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
