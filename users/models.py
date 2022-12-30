2dd9a3d27b5b9dc17b6b95f2e87ba07d9f352d5d
dennis <dennis@wsos.com.tw>
2022-12-30 11:33:49 +0800

    更新 帳號 models

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 import uuid
 # Create your models here.
@@ -12,3 +13,13 @@ class UserProfile(models.Model):
     class Meta:
         verbose_name = "頭像"
         verbose_name_plural = "頭像"
+
+
+class User(AbstractUser):
+    user   = models.OneToOneField(User, on_delete=models.CASCADE)
+    # avatar = models.ImageField(upload_to='avatar/%Y%m', blank=True, null=True, verbose_name='使用者頭像')
+    user_token = models.UUIDField(default=uuid.uuid4, unique=True, verbose_name='使用者公鑰')
+
+    class Meta:
+        verbose_name = "使用者資訊"
+        verbose_name_plural = "使用者資訊"
