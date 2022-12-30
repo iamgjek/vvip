8903d3d3ba7ca29bad150dfefda09414acddafdc
dennis <dennis@wsos.com.tw>
2022-12-30 11:48:55 +0800

    更新 User v123004

--- users/admin.py
+++ users/admin.py
@@ -1,23 +1,23 @@
-from django.contrib import admin
-# Register your models here.
-from users.models import UserProfile
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+# from django.contrib import admin
+# # Register your models here.
+# from users.models import UserProfile
+# from django.contrib.auth.models import User
+# from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
-    max_num = 1
-    can_delete = False
+# class UserProfileInline(admin.StackedInline):
+#     model = UserProfile
+#     max_num = 1
+#     can_delete = False
 
-class UserAdmin(AuthUserAdmin):
-    inlines = [UserProfileInline]
+# class UserAdmin(AuthUserAdmin):
+#     inlines = [UserProfileInline]
 
-# 註銷舊用戶管理員
-admin.site.unregister(User)
-# 註冊新用戶管理員
-admin.site.register(User, UserAdmin)
+# # 註銷舊用戶管理員
+# admin.site.unregister(User)
+# # 註冊新用戶管理員
+# admin.site.register(User, UserAdmin)
 
-@admin.register(UserProfile)
-class Share_receiptAdmin(admin.ModelAdmin):
-    list_display = ('user',)
+# @admin.register(UserProfile)
+# class Share_receiptAdmin(admin.ModelAdmin):
+#     list_display = ('user',)
