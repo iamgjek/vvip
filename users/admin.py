344791d82dc77a427650371c3d55511f77a41816
dennis <dennis@wsos.com.tw>
2022-12-30 14:39:57 +0800

    新增 User admin v123006

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,18 @@
-# from django.contrib import admin
-# # Register your models here.
+from django.contrib import admin
+from users import models
+
 # from users.models import UserProfile
 # from django.contrib.auth.models import User
 # from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=('username', 'last_login', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
+    fields = ['username', 'password', 'last_login', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser', 'date_joined', 'sub_domain']
+    search_fields = ['username']
+    readonly_fields = ['username']
+
+admin.site.register(models.User, UserAdmin)
 
 # class UserProfileInline(admin.StackedInline):
 #     model = UserProfile
--- users/models.py
+++ users/models.py
@@ -6,13 +6,15 @@ import uuid
 
 class User(AbstractUser):
     sub_domain = models.CharField(max_length=100, null=True, blank=True, verbose_name='子域')
-    # user   = models.OneToOneField(User, on_delete=models.CASCADE)
     # avatar = models.ImageField(upload_to='avatar/%Y%m', blank=True, null=True, verbose_name='使用者頭像')
     user_token = models.UUIDField(default=uuid.uuid4, unique=True, verbose_name='使用者公鑰')
 
     class Meta:
         verbose_name = "使用者資訊"
         verbose_name_plural = "使用者資訊"
+
+    def __str__(self):
+        return '{}'.format(self.username)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
